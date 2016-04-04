edcb3085b5f2ff9b7cd4b44a6ad87a4690394b8d
Kevin Linne <kevin.linne@mni.thm.de>
2016-04-04 15:13:28 +0200

    Displaying information in billDetailView (unstyled)

--- proSplit/client/routes.js
+++ proSplit/client/routes.js
@@ -51,6 +51,27 @@ Router.route('/event/:id',{
 Router.route('/event/:id/:bill', {
     template: 'billDetailView',
     data:function(){
-        return Events.findOne({_id:this.params.id}, {fields:{'bills':1}});
+
+        var billTitle = this.params.bill;
+        var doc = Events.findOne({_id:this.params.id}, {fields:{'bills':1}});
+        var doc2 = {title: "default"};
+        //return doc;
+        if(doc) {
+            doc.bills.forEach(function (value) {
+                if (value.title == billTitle) {
+                    doc2 = {
+                        title: value.title,
+                        amount: value.amount,
+                        payer: value.payer,
+                        receiver: value.receiver
+                    };
+
+                    console.log(doc2);
+
+                }
+            });
+        }
+        console.log("Schleifenende");
+        return doc2;
     }
-})
+});
