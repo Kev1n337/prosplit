a4eaa9fd53045b586d45e0bf5be7a56cc1548a06
Kevin Linne <kevin.linne@mni.thm.de>
2016-04-05 21:57:55 +0200

    Displaying balance for event

--- proSplit/client/routes.js
+++ proSplit/client/routes.js
@@ -1,5 +1,5 @@
 /**
- * Created by Kevin on 13.03.16.
+ * Created by  on 13.03.16.
  */
 Router.configure({
     layoutTemplate: 'layout'
@@ -51,7 +51,66 @@ Router.route('/event/:id',{
     //layout:'layout',
     template: 'eventDetailView',
     data:function(){
-        return Events.findOne({_id:this.params.id});
+        var event = Events.findOne({_id:this.params.id});
+
+        var user = [];
+        var balance = [];
+
+        if(event) {
+            if($.inArray(event.owner, user) < 0) {
+                user.push(event.owner);
+            }
+
+            $.each(event.bills, function(key,bill){
+                if($.inArray(bill.payer, user) < 0) {
+                    user.push(bill.payer);
+                }
+                $.each(bill.receiver, function(key, receiver){
+                    if($.inArray(receiver, user) < 0) {
+                        user.push(receiver);
+                    }
+                });
+
+            });
+            user.sort();
+            $.each(user, function(){
+                balance.push({name: this, actBalance:0});
+            });
+
+            $.each(event.bills, function(key, bill){
+
+                $.each(balance, function(index, memb){
+                    if(this.name==bill.payer){
+                        memb.actBalance += bill.amount;
+                    }
+
+                    $.each(bill.receiver, function(index, receiver){
+                        if(memb.name==receiver){
+                            var amountPerReceiver = bill.amount / bill.receiver.length;
+                            memb.actBalance -= amountPerReceiver;
+                        }
+                    });
+
+                    memb.actBalance = Number((memb.actBalance).toFixed(2)); //Round
+
+                    if(memb.actBalance >= 0) {
+                        memb.class = "green-text";
+                    } else {
+                        memb.class = "red-text";
+                    }
+                });
+
+            });
+        }
+
+        var ret = {
+            event: event,
+            member: user,
+            balance: balance
+        };
+
+
+        return ret;
     }
 });
 
