7c011ea474cbec94afbea46578f5be70c9d205d9
Kevin Linne <kevin.linne@mni.thm.de>
2016-04-08 19:55:13 +0200

    Equationbills for all Events

--- proSplit/client/js/userView.js
+++ proSplit/client/js/userView.js
@@ -3,6 +3,7 @@
  */
 Deps.autorun(function(){
     Meteor.subscribe('User.userdata');
+    Meteor.subscribe('Events.eventdata');
 });
 
 Template.userView.rendered = function(){
@@ -18,8 +19,51 @@ Template.userView.rendered = function(){
 
 Template.userView.helpers({
     friends: function(){
-        if(Meteor.user()) {
-            return Meteor.user().friends;
+        var contacts = [];
+
+        if(Meteor.user() && Meteor.user().friends) {
+            var friends = Meteor.user().friends;
+            $.each(friends, function(){
+                contacts.push({name: this, amount:0});
+            });
+
+            //var user = Meteor.users.findOne({username:Meteor.user().username});
+
+            console.log(Meteor.user());
+
+            if(Meteor.user() && Meteor.user().events) {
+                $.each(Meteor.user().events, function () {
+                    var event = Events.findOne({_id: this.toString()});
+                    $.each(event.eqBills, function (i, bill) {
+                        if (Meteor.user().username == bill.to) {
+                            $.each(contacts, function () {
+                                if (this.name == bill.from) {
+                                    this.amount += bill.amount;
+                                }
+                            });
+                        }
+                        if (Meteor.user().username == bill.from) {
+                            $.each(contacts, function () {
+                                if (this.name == bill.to) {
+                                    this.amount -= bill.amount;
+                                }
+                            });
+                        }
+                    });
+                });
+            }
+
+            $.each(contacts, function(){
+                if(this.amount < 0) {
+                    this.cssClass = "red-text";
+                } else {
+                    this.cssClass = "green-text";
+                }
+
+                console.log(this);
+            });
+
+            return contacts;
         }
         return [];
     }
